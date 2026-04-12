2953723e00632e5747400b4a0dd9a0bb3d5c72d3
ultimateblanks-collab <ultimateblanks@gmail.com>
2026-04-12 17:11:50 +0900

    Update paw3222.c

--- src/paw3222.c
+++ src/paw3222.c
@@ -272,14 +272,14 @@ speed = smooth_speed;
 
 // パラメータ
 int32_t min_accel = 400;
-int32_t max_accel = 2000;
-int32_t offset = 180;
+int32_t max_accel = 3000;
+int32_t offset = 150;
 
 int32_t accel;
 
 // 低速専用
 if (speed < 120) {
-    accel = 380;
+    accel = 400;
 } else {
     accel = min_accel +
         ((max_accel - min_accel) * speed) / (speed + offset);
